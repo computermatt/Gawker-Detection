d6d0113bd86ca49062df6a319a1c5f9caaccd1b0
Matt <matt@mattsapps.com>
2010-07-25 17:58:11 -0400

    First (and probably last) commit

---
+++ GawkerDetection.safariextension/detection.js
@@ -0,0 +1,41 @@
+
+if (window.top === window) {
+
+
+ var itemUrl =  window.location.href;
+
+ if (itemUrl === "http://www.gizmodo.com/")
+ { 
+    alert("WARNING: YOU ARE ABOUT TO VISIT A SITE OWNED BY GAWKER MEDIA.");
+}
+if (itemUrl === "http://gawker.com/")
+ { 
+    alert("WARNING: YOU ARE ABOUT TO VISIT A SITE OWNED BY GAWKER MEDIA.");
+}
+if (itemUrl === "http://deadspin.com/")
+ { 
+    alert("WARNING: YOU ARE ABOUT TO VISIT A SITE OWNED BY GAWKER MEDIA.");
+}
+if (itemUrl === "http://kotaku.com/")
+ { 
+    alert("WARNING: YOU ARE ABOUT TO VISIT A SITE OWNED BY GAWKER MEDIA.");
+}
+if (itemUrl === "http://jezebel.com/")
+ { 
+    alert("WARNING: YOU ARE ABOUT TO VISIT A SITE OWNED BY GAWKER MEDIA.");
+}
+if (itemUrl === "http://io9.com/")
+ { 
+    alert("WARNING: YOU ARE ABOUT TO VISIT A SITE OWNED BY GAWKER MEDIA.");
+}
+if (itemUrl === "http://jalopnik.com/")
+ { 
+    alert("WARNING: YOU ARE ABOUT TO VISIT A SITE OWNED BY GAWKER MEDIA.");
+}
+if (itemUrl === "http://lifehacker.com/")
+ { 
+    alert("WARNING: YOU ARE ABOUT TO VISIT A SITE OWNED BY GAWKER MEDIA.");
+}
+
+
+}
